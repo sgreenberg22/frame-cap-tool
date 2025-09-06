ef791740f7c5b0b6c474afc28c52244916dcce8c
sgreenberg22 <81115218+sgreenberg22@users.noreply.github.com>
2025-09-06 00:35:00 -0500

    Update Analysis.jsx

--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -81,6 +81,9 @@ export default function Analysis(){
 
   return (
     <div className="container">
+      <div className="container">
+  <Link to="/">Home</Link>
+</div>
       <section className="grid">
         <div className="card">
           <details open>
