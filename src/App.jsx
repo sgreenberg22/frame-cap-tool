193e08a42657b56c4a4cb861ab7050daf2d41b2e
sgreenberg22 <81115218+sgreenberg22@users.noreply.github.com>
2025-09-06 00:32:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const loc = useLocation()
 return (
 <>
 <div className="container">
+  <Link to="/">Home</Link>
 </div>
 <Outlet key={loc.pathname} />
 <footer className="container" style={{marginTop:24}}>
