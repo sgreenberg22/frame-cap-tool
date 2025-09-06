45db84be595baed59d77bd77485231a2361aff80
sgreenberg22 <81115218+sgreenberg22@users.noreply.github.com>
2025-09-06 00:51:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ return (
 </div>
 <Outlet key={loc.pathname} />
 <footer className="container" style={{marginTop:24}}>
-<small>Built by Stephen G. <a href="https://buymeacoffee.com/scgreenberg">Buy me a coffee?</a></small>
+<small>Built by Stephen G. <a href="https://buymeacoffee.com/scgreenberg" target="_blank">Buy me a coffee?</a></small>
 </footer>
 </>
 )
